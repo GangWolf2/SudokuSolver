e6c1cf236d142638cb6dc7b5cec1e7edc0a958cf
GangWolf2 <32149417+GangWolf2@users.noreply.github.com>
2020-07-19 09:21:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#7/19/20
+#Wulfgang Rankenburg
+#Based off of Tech From Tim Sudoku soultion finder, different implementation
+#Given a standard 9x9 Sudoku board, this program utilizes backtracking to find a valid solution
+#To any solvable 9x9 sudoku board, otherwise it returns the original board if unsolvable
+
 from solution import *
 import time
 def main():
@@ -16,30 +22,11 @@ def main():
         [3,0,0,8,0,6,0,9,0],
         [0,4,2,7,0,0,0,0,3]
     ]
-
-    board2 = [
-        [0,0,5,0,1,0,6,0,0],
-        [3,9,0,7,0,0,0,1,0],
-        [4,0,7,9,0,0,0,2,8],
-        [9,7,0,8,0,0,0,4,0],
-        [5,0,0,2,0,6,0,0,1],
-        [0,8,0,0,0,1,0,7,3],
-        [2,6,0,0,0,8,7,0,4],
-        [0,5,0,0,0,7,0,3,9],
-        [0,0,8,0,4,0,1,0,0]
-    ]
-
-    board3 = [
-        []
-    ]
+    
     start_time = time.time()
     drawBoard(board)
     solve(board)
     drawBoard(board)
     print("Finished in --- %s seconds ---" % (time.time() - start_time))
 
-    drawBoard(board2)
-    solve(board2)
-    drawBoard(board2)
-    print("Finished in --- %s seconds ---" % (time.time() - start_time))
 main()
